4c12992ff2b0abc01883ce346e1566dd0278e1ab
Aglae Verduzco <aglae.cv@gmail.com>
2019-03-13 01:04:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,11 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-  self.all.detect {|artist| artist.name == name} ||  Artist.new(name)
+  self.find do |artist|
+    if artist.name == name
+      artist
+    else
+      new_artist = Artist.new(name)
   end
 
   def print_songs
